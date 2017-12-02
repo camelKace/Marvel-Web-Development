13e6583029ef2d3243734ea9c017cc808ffe7a61
kaceyjimenez <kaceyjimenez21@gmail.com>
2017-12-02 15:21:47 -0500

    Adding web dir

--- web/app.js
+++ web/app.js
@@ -5,7 +5,7 @@ let path = require('path')
     app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/dom.html'));
 })
-
+app.use('/', express.static(path.join(__dirname+'/')))
 app.listen('3000', () => {
   console.log('express is listening on port 3000')
 })
